56412ee40dad084b000aa1ef7cc3c77b59d309d4
Mihai M <mmarinescu@192-168-0-102.rdsnet.ro>
2017-09-12 23:37:43 +0300

    some mongoose play

---
+++ starting.js
@@ -0,0 +1,41 @@
+module.exports = () => {
+    // getting-started.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('we are connected');
+});
+
+var kittySchema = mongoose.Schema({
+    name: String
+});
+
+kittySchema.methods.speak = function () {
+  var greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name";
+  console.log(greeting);
+}
+
+var Kitten = mongoose.model('Kitten', kittySchema);
+
+var silence = new Kitten({ name: 'Silence' });
+console.log(silence.name); // 'Silence'
+
+var fluffy = new Kitten({ name: 'fluffy' });
+fluffy.speak();
+
+fluffy.save(function (err, fluffy) {
+  if (err) return console.error(err);
+  fluffy.speak();
+});
+
+Kitten.findOne(function (err, kittens) {
+  if (err) return console.error(err);
+  console.log(kittens);
+})
+}
